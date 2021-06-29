68bd0cfe2a1e83f5e3e1b7ebcc6f2d6a8af98f3e
ShatterPlayer <shatterplayer@gmail.com>
2021-06-29 11:13:53 +0200

    Configurated Redux DevTools

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger)),
+)
 
 export default store
