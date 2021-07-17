3866834767681c62576fd9d021b31dba694b6c7a
ShatterPlayer <shatterplayer@gmail.com>
2021-07-17 13:26:49 +0200

    Added new allowed characters to regexes

--- src/utils/regexes.js
+++ src/utils/regexes.js
@@ -2,8 +2,8 @@ export default {
   regexUsername: /^[a-z0-9żźćńółęąśŻŹĆĄŚĘŁÓŃ\s]{3,20}$/i,
   regexCorrectAnswer: /^[0-3]$/,
   regexId: /^[0-9a-f]{24}$/,
-  regexQuizTitle: /^[a-z0-9żźćńółęąśŻŹĆĄŚĘŁÓŃ\s]{4,70}$/i,
-  regexQuestionText: /^[a-z0-9'żźćńółęąśŻŹĆĄŚĘŁÓŃ\s]{1,100}$/i,
-  regexQuestionAnswer: /^[a-z0-9żźćńółęąśŻŹĆĄŚĘŁÓŃ\s]{1,70}$/i,
+  regexQuizTitle: /^[a-z0-9'żźćńółęąśŻŹĆĄŚĘŁÓŃ?!.,<>/[\])(;:"#$%^&*+-_\s]{4,70}$/i,
+  regexQuestionText: /^[a-z0-9'żźćńółęąśŻŹĆĄŚĘŁÓŃ?!.,<>/[\])(;:"#$%^&*+-_\s]{1,100}$/i,
+  regexQuestionAnswer: /^[a-z0-9'żźćńółęąśŻŹĆĄŚĘŁÓŃ?!.,<>/[\])(;:"#$%^&*+-_\s]{1,70}$/i,
   regexQuestionNumber: /^[0-9]+$/,
 }
