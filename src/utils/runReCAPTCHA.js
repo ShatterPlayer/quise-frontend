36b6908ade63cc58e0fdd9bad6e2c7a45582bfde
ShatterPlayer <shatterplayer@gmail.com>
2021-07-20 13:49:46 +0200

    Reduced ReCAPTCHA validation time

--- src/utils/runReCAPTCHA.js
+++ src/utils/runReCAPTCHA.js
@@ -4,6 +4,7 @@ const runReCAPTCHA = async recaptchaRef => {
   try {
     store.dispatch({ type: CLEAR_ERROR })
     const token = await recaptchaRef.current.executeAsync()
+    recaptchaRef.current.reset()
     return token
   } catch {
     store.dispatch(addError('ReCAPTCHA validation failed. Try again later.'))
