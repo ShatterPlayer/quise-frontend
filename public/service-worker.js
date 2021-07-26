516dbc59b8673e2628342ebb9c9b59bea5ccad02
ShatterPlayer <shatterplayer@gmail.com>
2021-07-26 15:36:52 +0200

    Renamed cache and fixes small bug

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,12 @@
+const VERSION = '1'
+const CACHE_NAME = `offline-${VERSION}`
+
 self.addEventListener('install', function (evt) {
   console.log('Installing service worker')
 
   evt.waitUntil(
-    caches.open('test').then(function (cache) {
-      cache.add('/offline')
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.add('/offline')
     }),
   )
 
@@ -13,7 +16,7 @@ self.addEventListener('install', function (evt) {
 self.addEventListener('fetch', function (evt) {
   evt.respondWith(
     fetch(evt.request).catch(function () {
-      return caches.open('test').then(function (cache) {
+      return caches.open(CACHE_NAME).then(function (cache) {
         return cache.match('/offline')
       })
     }),
