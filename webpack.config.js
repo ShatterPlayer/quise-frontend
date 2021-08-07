0694c248f72c4addad26f56bb43a9a8871e8b370
ShatterPlayer <shatterplayer@gmail.com>
2021-08-07 16:37:33 +0200

    Removed Create React App utility

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: ['regenerator-runtime/runtime.js', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.[contenthash].js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        include: /src/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.svg/,
+        include: /src/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+}
